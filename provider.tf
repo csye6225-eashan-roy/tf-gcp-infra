ea5fa67133e1a2af681bc0f39d6606a2b40377c2
Eashan Roy <roy.ea@northeastern.edu>
2024-02-12 00:27:23 -0500

    created vpc

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file_path)
+  // credentials of service account dedicated to Terraform
+  project     = "csye6225-eashan-roy"
+  region      = "us-central1"
+}
