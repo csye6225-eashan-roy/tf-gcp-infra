4c61ebc872c286815463e131e57dc418ac018970
Eashan Roy <roy.ea@northeastern.edu>
2024-02-20 02:24:18 -0500

    removed all hardcoded values

--- variables.tf
+++ variables.tf
@@ -4,29 +4,32 @@ resource "random_string" "resource_name" {
   upper   = false # in GCP, names must not include uppercase letters.
 }
 
-variable "webapp-internet-access" {
+
+##############################################################
+
+#                   VARIABLES - VPC
+
+##############################################################
+
+variable "vpc-webapp-internet-access" {
   type = string
 }
 
-variable "db-subnet-cidr" {
+variable "vpc-db-subnet-cidr" {
   type = string
 }
 
-variable "routing-mode" {
+variable "vpc-routing-mode" {
   type = string
 }
 
-variable "webapp-subnet-cidr" {
+variable "vpc-webapp-subnet-cidr" {
   type = string
 }
 
-variable "region" {
+variable "vpc-region" {
   type = string
 }
-
-# variable "credentials_file_path" {
-#     type = string
-# }
 
 variable "protocol" {
   type = string
@@ -44,13 +47,102 @@ variable "port-ssh" {
   type = string
 }
 
-# variable "ports" {
-#   type    = list(string)
-#   default = ["80", "443", "22"] // HTTP and HTTPS
-# }
-
 variable "source_ranges" {
   description = "CIDR IP ranges that are allowed or denied"
   type        = list(string)
 }
+
+variable "vpc-auto-create-subnets" {
+  type = bool
+}
+
+variable "vpc-delete-default-routes" {
+  type = bool
+}
+
+variable "vpc-next-hop-gateway" {
+  type = string
+}
+
+variable "firewall-allow-https-tag" {
+  type = string
+}
+
+variable "firewall-allow-http-tag" {
+  type = string
+}
+
+variable "firewall-allow-ssh-tag" {
+  type = string
+}
+
+variable "firewall-deny-http-tag" {
+  type = string
+}
+
+variable "firewall-deny-https-tag" {
+  type = string
+}
+
+##############################################################
+
+#                    VARIABLES - VM
+
+##############################################################
+
+variable "vm-name" {
+  type = string
+}
+
+variable "vm-size" {
+  type = string
+}
+
+variable "vm-zone" {
+  type = string
+}
+
+variable "packer-image-path" {
+  type = string
+}
+
+variable "service-account-email" {
+  type = string
+}
+
+variable "service-account-scope" {
+  type = string
+}
+
+variable "vm-tags" {
+  type = list(string)
+}
+
+variable "vm-can-ip-forward" {
+  type = bool
+}
+
+variable "vm-deletion-protection" {
+  type = bool
+}
+
+variable "vm-storage" {
+  type = string
+}
+
+
+##############################################################
+
+#                    VARIABLES - PROVIDER
+
+##############################################################
+
+variable "project-id" {
+  type = string
+}
+
+variable "provider-region" {
+  type = string
+}
+
 
