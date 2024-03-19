3d1b4656d03f785640f5bee39e1fc099b8104d18
Eashan Roy <roy.ea@northeastern.edu>
2024-03-19 04:35:39 -0400

    updated hardcoded values with variables

--- dns.tf
+++ dns.tf
@@ -1,8 +1,8 @@
 resource "google_dns_record_set" "a_record" {
-  name         = "eashanroy.me."
-  type         = "A"
-  ttl          = 21600
-  managed_zone = "eashanroy"
+  name         = var.dns_name
+  type         = var.dns_type
+  ttl          = var.dns_ttl
+  managed_zone = var.managed_zone
   rrdatas      = [google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip]
 
   depends_on = [google_compute_instance.vm_instance]
