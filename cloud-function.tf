ee826d23d8941e538cec9c3b07cbd1633c054c5f
Eashan Roy <roy.ea@northeastern.edu>
2024-03-27 02:32:25 -0400

    terraform fmt

--- cloud-function.tf
+++ cloud-function.tf
@@ -23,13 +23,13 @@ resource "google_cloudfunctions2_function" "function" {
     min_instance_count = 1
     available_memory   = var.cloud_function_available_memory
     timeout_seconds    = var.cloud_function_timeout_seconds
-    vpc_connector = google_vpc_access_connector.vpc_connector.id
+    vpc_connector      = google_vpc_access_connector.vpc_connector.id
     environment_variables = {
       MAILGUN_API_KEY = "75dcdc418ace512651bc4c2663d4dcb7-f68a26c9-ec0e60c0",
-      DB_USER = google_sql_user.db_user.name,
-      DB_PASS = random_password.db_password.result,
-      DB_NAME = google_sql_database.webapp_database.name,
-      DB_HOST = google_sql_database_instance.cloudsql_instance.private_ip_address
+      DB_USER         = google_sql_user.db_user.name,
+      DB_PASS         = random_password.db_password.result,
+      DB_NAME         = google_sql_database.webapp_database.name,
+      DB_HOST         = google_sql_database_instance.cloudsql_instance.private_ip_address
     }
     service_account_email = google_service_account.cloud_function_account.email
   }
